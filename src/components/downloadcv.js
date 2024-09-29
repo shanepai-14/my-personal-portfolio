d0ecf27b358af58ee5155419f5e0a0336fead123
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-09-30 03:05:32 +0800

    Update downloadcv.js

--- src/components/downloadcv.js
+++ src/components/downloadcv.js
@@ -3,7 +3,6 @@ import { MdOutlineFileDownload } from "react-icons/md";
 import cv from "../documents/Shane F. Enriquez CV.pdf";
 
 const handleDownloadCV = () => {
-  // Replace 'my-cv.pdf' with the actual name of your CV file
   const cvUrl = cv;
   const link = document.createElement("a");
   link.href = cvUrl;
